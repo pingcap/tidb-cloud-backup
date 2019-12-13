49e9a56463b28a8b376074fe69c1b3cdfa476eba
Jacob Lerche <jacob.lerche@gmail.com>
2019-12-12 16:17:31 -0800

    Fix space

--- backup.sh
+++ backup.sh
@@ -60,7 +60,7 @@ if [ "${CLOUD}" = "ceph" ]; then
 fi
 
 if [ "${CLOUD}" = "aws" ]; then
-    if [! -z "${AWS_ACCESS_KEY_ID}" ] && [ ! -z "${AWS_SECRET_ACCESS_KEY}" ]; then
+    if [ ! -z "${AWS_ACCESS_KEY_ID}" ] && [ ! -z "${AWS_SECRET_ACCESS_KEY}" ]; then
         export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
         export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
     fi
